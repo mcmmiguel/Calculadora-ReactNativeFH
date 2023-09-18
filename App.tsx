363cc173a0a6236c25a4ad280fbcba286df9aa09
Miguel Cobian <miguelcobian99@gmail.com>
2023-09-18 13:23:41 -0600

    Project started

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import React from 'react';
+import CalculadoraScreen from './src/screens/CalculadoraScreen';
+
+const App = () => {
+  return (
+    <View>
+      <CalculadoraScreen />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const CalculadoraScreen = () => {
+    return (
+        <View>
+            <Text>Hola mundo Calculadora</Text>
+        </View>
+    );
+};
+
+export default CalculadoraScreen;
